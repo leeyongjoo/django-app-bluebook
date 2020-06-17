27e2965cf0dc4e1f98f508c5034c4c897fa48315
leeyongjoo <2yongjooo@gmail.com>
2020-06-17 21:26:24 +0900

    수정: 월별로 요청할 때 월을 int로 요청

--- blog/urls.py
+++ blog/urls.py
@@ -21,10 +21,10 @@ urlpatterns = [
     path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
 
     # Example: /blog/archive/2019/nov/
-    path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
+    path('archive/<int:year>/<int:month>/', views.PostMAV.as_view(month_format='%m'), name='post_month_archive'),
 
     # Example: /blog/archive/2019/nov/10/
-    path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
+    path('archive/<int:year>/<int:month>/<int:day>/', views.PostDAV.as_view(month_format='%m'), name='post_day_archive'),
 
     # Example: /blog/archive/today/
     path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
