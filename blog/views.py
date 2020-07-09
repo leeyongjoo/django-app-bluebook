3fbcbff9fbdb031fe4d28fa3606cf263b4600098
leeyongjoo <2yongjooo@gmail.com>
2020-07-09 23:23:38 +0900

    추가: 검색 기능 페이지 추가

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic import DayArchiveView, TodayArchiveView
+from django.views.generic import FormView
 
 from blog.models import Post
 from django.conf import settings
+from blog.forms import PostSearchForm
+from django.db.models import Q
+from django.shortcuts import render
 
 #--- ListView
 class PostLV(ListView):
@@ -66,3 +70,21 @@ class TaggedObjectLV(ListView):
         context = super().get_context_data(**kwargs)
         context['tagname'] = self.kwargs['tag']
         return context
+
+#--- FormView
+class SearchFormView(FormView):
+    form_class = PostSearchForm
+    template_name = 'blog/post_search.html'
+
+    def form_valid(self, form):
+        searchWord = form.cleaned_data['search_word']
+        post_list = Post.objects.filter(Q(title__icontains=searchWord)
+                                        |Q(description__icontains=searchWord)
+                                        |Q(content__icontains=searchWord)).distinct()
+
+        context = {}
+        context['form'] = form
+        context['search_term'] = searchWord
+        context['object_list'] = post_list
+
+        return render(self.request, self.template_name, context)    # No Redirection
